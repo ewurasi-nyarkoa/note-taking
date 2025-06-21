ecdf8541fd7fb27784a843a6ca3da9c100ed1941
ewurasi nyarkoa <anewurasi@gmail.com>
2025-06-21 11:35:51 +0000

    created pages component to handle routes configured and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ArchivedComponent } from './pages/archived/archived.component';
+import { NoteDetailComponent } from './pages/note-detail/note-detail.component';
+import { CreateNoteComponent } from './pages/create-note/create-note.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/notes', pathMatch: 'full' },
+  { path: 'notes', component: DashboardComponent },
+  { path: 'archived', component: ArchivedComponent },
+  { path: 'notes/:id', component: NoteDetailComponent },
+  { path: 'create', component: CreateNoteComponent }
+];
