1b21d87442a2d5200678233ad00077d971b6a780
ewurasi nyarkoa <anewurasi@gmail.com>
2025-06-23 21:13:47 +0000

    created a dashboard that displaces all active notes with full search functionality

--- src/app/components/note-list/note-list.component.ts
+++ src/app/components/note-list/note-list.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Note } from '../../models/note.interface';
+import { NoteService } from '../../services/note.service';
 
 @Component({
   selector: 'app-note-list',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './note-list.component.html',
   styleUrl: './note-list.component.scss'
 })
 export class NoteListComponent {
+  @Input() notes: Note[] = [];
+  @Input() showArchived = false;
 
+  constructor(private noteService: NoteService) {}
+
+  toggleArchive(note: Note) {
+    if (note.isArchived) {
+      this.noteService.updateNote(note.id, { isArchived: false });
+    } else {
+      this.noteService.archiveNote(note.id);
+    }
+  }
+
+  deleteNote(note: Note) {
+    this.noteService.deleteNote(note.id);
+  }
+
+  truncateContent(content: string): string {
+    return content.length > 100 ? content.substring(0, 100) + '...' : content;
+  }
 }
