508c10a8d9bc0f4dcaf47ae83c1ace8cc4f2beeb
ewurasi nyarkoa <anewurasi@gmail.com>
2025-06-25 15:10:07 +0000

    worked on the detail page

--- src/app/pages/note-detail/note-detail.component.ts
+++ src/app/pages/note-detail/note-detail.component.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NoteService } from '../../services/note.service';
+import { DatabaseNote } from '../../models/note.interface';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-note-detail',
-  imports: [],
+  imports: [CommonModule, SidebarComponent],
   templateUrl: './note-detail.component.html',
   styleUrl: './note-detail.component.scss'
 })
-export class NoteDetailComponent {
+export class NoteDetailComponent implements OnInit {
+  note: DatabaseNote | null = null;
+  isLoading = true;
+  allTags: string[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private noteService: NoteService
+  ) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.loadNote(+id);
+    }
+  }
+
+  loadNote(id: number) {
+    this.noteService.getNotes().subscribe(notes => {
+      this.note = notes.find(n => n.id === id) || null;
+      this.updateTags(notes);
+      this.isLoading = false;
+    });
+  }
+
+  private updateTags(notes: DatabaseNote[]) {
+    const tagSet = new Set<string>();
+    notes.forEach(note => note.tags.forEach(tag => tagSet.add(tag)));
+    this.allTags = Array.from(tagSet);
+  }
+
+  onTagFilter(tag: string) {
+    // Navigate to dashboard with tag filter
+    this.router.navigate(['/dashboard'], { queryParams: { tag } });
+  }
+
+  editNote() {
+    if (this.note) {
+      this.router.navigate(['/edit', this.note.id]);
+    }
+  }
+
+  async toggleArchive() {
+    if (this.note) {
+      await this.noteService.updateNote(this.note.id, { is_archived: !this.note.is_archived });
+    }
+  }
+
+  async deleteNote() {
+    if (this.note && confirm('Are you sure you want to delete this note?')) {
+      await this.noteService.deleteNote(this.note.id);
+      this.router.navigate(['/dashboard']);
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/dashboard']);
+  }
 }
